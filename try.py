601ea116303625afb8d33b69da1b1aa99c29650d
WanHaoRan <wanhr@outlook.com>
2019-04-04 04:09:01 -0900

    do nothing

--- try.py
+++ try.py
@@ -1,16 +1,35 @@
-import numpy as np
-import scipy.signal as signal
-import matplotlib .pyplot as plt
+# Implements an in-memory CIC decimator using numpy.
 
-b1 = signal.firwin(40,0.9)
-#b2 = signal.firwin(41,[0.3,0.8])
-w1,h1 = signal.freqz(b1)
-#w2,h2 = signal.freqz(b2)
+from math import log
+from numpy import int32, int64, array
 
-plt.title('Digital filter frequency response')
-plt.plot(w1,20*np.log10(np.abs(h1)),'b')
-#plt.plot(w2,20*np.log10(np.abs(h2)),'r')
-plt.ylabel('Amplitude Response (dB)')
-plt.xlabel('Frequency (rad/sample)')
-plt.grid()
-plt.show()
+def cic_decimator(source, decimation_factor=32, order=5, ibits=1, obits=16):
+
+    # Calculate the total number of bits used internally, and the output
+    # shift and mask required.
+    numbits = order * int(round(log(decimation_factor) / log(2))) + ibits
+    outshift = numbits - obits
+    outmask  = (1 << obits) - 1
+
+    # If we need more than 64 bits, we can't do it...
+    assert numbits <= 64
+
+    # Create a numpy array with the source
+    result = array(source, int64 if numbits > 32 else int32)
+
+    # Do the integration stages
+    for i in range(order):
+        result.cumsum(out=result)
+
+    # Decimate
+    result = array(result[decimation_factor - 1 : : decimation_factor])
+
+    # Do the comb stages.  Iterate backwards through the array,
+    # because we use each value before we replace it.
+    for i in range(order):
+        result[len(result) - 1 : 0 : -1] -= result[len(result) - 2 : : -1]
+
+    # Normalize the output
+    result >>= outshift
+    result &= outmask
+    return result
